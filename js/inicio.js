6b1e5690bee6fd0d2613fb96aeab37753fe72995
carlos anibal gabrovich <carlos.gabrovich@gmail.com>
2025-10-16 21:54:11 -0300

    correcciones en localstorage y sesionstorage

--- js/inicio.js
+++ js/inicio.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  if (localStorage.getItem("medicos") === null) {
+    console.log('entro a modificar el localstorage,linea 3 inicio.js')
+    fetch("json/medicos.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new "error al cargar el archivo"();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("medicos", JSON.stringify(data));
+        /* console.log(typeof data); */
 
-fetch('json/medicos.json')
-.then(response=>{
-  if(!response.ok) {
-    throw new ("error al cargar el archivo");
-  }
-  return response.json();
-        })
-        .then(data=>{
-          localStorage.setItem("medicos",JSON.stringify(data));
-          console.log(typeof(data))
-
-          let datos=localStorage.getItem('medicos');
-        console.log(typeof(JSON.parse(datos)));
-        let j=JSON.parse(datos);
-        console.log(j);
-            j["0"].medicos.forEach((element) => {
-              console.log(element.nombre)
-          let cards= document.getElementById('cards')
-        cards.innerHTML += `
+        let datos = localStorage.getItem("medicos");
+        /* console.log(typeof JSON.parse(datos)); */
+        let j = JSON.parse(datos);
+        /* console.log(j); */
+        j["0"].medicos.forEach((element) => {
+          /* console.log(element.nombre); */
+          let cards = document.getElementById("cards");
+          cards.innerHTML += `
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
         <div class="card h-100 text-center">
           <img
@@ -31,20 +32,13 @@ fetch('json/medicos.json')
             <p class="card-text">Especialidad: ${element.especialidad}</p>
           </div>
         </div>
-      </div>`
-            
-            });
-        })
-
-
-        sessionStorage.setItem('usuario',"paciente");
-       
-        
-
-        
-            });
-
-            
-      
-        
-    
+      </div>`;
+        });
+      });
+  }
+if (sessionStorage.getItem('usuario') === null)
+{
+  console.log('entro a modificar el sesionstorage,linea 40 inicio.js');
+  sessionStorage.setItem("usuario", "paciente");
+  }
+});
