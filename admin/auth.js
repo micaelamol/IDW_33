0d19c6f8421410519e97f68b2f59abb905343ad3
[Sebastian] <[sebastian_maiden@hotmail.com]>
2025-11-11 07:39:26 -0300

    Actualiza lógica de autenticación en admin/auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -8,22 +8,21 @@ export async function login(username, password) {
     });
 
     console.log("HTTP status:", response.status);
-    // Opcional: ver header Content-Type devuelto
+    
     console.log("Response headers:", response.headers.get("content-type"));
 
     const data = await response.json();
-    /*console.log("Respuesta API login:", data);*/
+    
     console.log("Respuesta API login:", JSON.stringify(data, null, 2));
 
     if (!response.ok) {
-      // devuelve un error controlado con la info de la API
       throw new Error(data.message || `Login falló (status ${response.status})`);
     }
 
     return data;
   } catch (error) {
     console.error("Error en login (auth.js):", error);
-    // Re-lanzamos el error para que el caller lo capture y muestre mensaje
+
     throw error;
   }
 }
