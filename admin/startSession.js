e27b7f85f3449b3fc45baf8849fade5fda518f04
carlos anibal gabrovich <carlos.gabrovich@gmail.com>
2025-11-11 11:20:54 -0300

    correccion de error startsession.js

--- admin/startSession.js
+++ admin/startSession.js
@@ -1,13 +1,17 @@
-export function startSession(token,username){
-  fetch('https://dummyjson.com/auth/me', {
-  method: 'GET', /* or POST/PUT/PATCH/DELETE */
-  headers: {
-    'Authorization': `Bearer ${token}`, 
-    'Content-Type': 'application/json'
-  }, 
-  
-})
-.then(res => res.json())
-.then(data=>{return data.username === username });
-
+export function startSession(token, username) {
+  try {
+   return fetch("https://dummyjson.com/auth/me", {
+      method: "GET" /* or POST/PUT/PATCH/DELETE */,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        return data.username === username;
+      });
+  } catch (e) {
+    throw new Error(`Error al iniciar sesión: ${e.message}`);
+  }
 }
